914bb9d8eb7be9dda3bb4cc8ae3416192edbf7c2
Austin Bingham <austin.bingham@gmail.com>
2013-11-23 11:18:16 +0100

    Added empty-list check to wpt:pop

--- wpt/wpt.el
+++ wpt/wpt.el
@@ -46,12 +46,15 @@
 (defun wpt:push ()
   "Push the current position to the front of the queue."
   (interactive)
-  (set-variable 'wpt:positions (cons (list (buffer-name) (point))
-                                  wpt:positions)))
+  (set-variable 'wpt:positions
+                (cons (list (buffer-name) (point))
+                      wpt:positions)))
 
 (defun wpt:pop ()
   "Pop a position from the queue and jump to it."
   (interactive)
+  (unless wpt:positions
+    (error "No waypoints to pop!"))
   (let* ((address (car wpt:positions))
          (buff (car address))
          (pt (cadr address))
